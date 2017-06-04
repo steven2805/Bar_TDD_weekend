b9108f74a55c7f9e935b697559058813c9257197
steven2805 <stevenmac90@googlemail.com>
2017-06-04 16:43:33 +0100

    Got the bar working correctly

--- bar.rb
+++ bar.rb
@@ -1,14 +1,26 @@
 class Bar 
 
-  attr_reader(:drink1,:drink2,:drink3,:drink4,:bar_profits)
+  attr_reader(:drinks,:drink2,:drink3,:drink4,:bar_profits)
 
-  def initialize(drink1,drink2,drink3,drink4)
+  def initialize
+    @drinks = {}
+    @bar_profits = 0
+  end 
 
-    @drink1 = drink1
-    @drink2 = drink2
-    @drink3 = drink3
-    @drink4 = drink4
-    @bar_profits = 0 
+  def add_drink_to_drinks_list(drink_name, price)
+    @drinks[drink_name] = price
+  end
+
+  def adding_sale_to_profits(drink_name)
+    if @drinks[drink_name]
+      @bar_profits += @drinks[drink_name]
+   
+    end
 
   end 
+
+
+
+
+
 end 
--- spec/bar_spec.rb
+++ spec/bar_spec.rb
@@ -5,15 +5,32 @@ require_relative('../bar')
 class TestBar < MiniTest::Test
 
   def setup 
-    @bar1 = Bar.new(100 ,50 ,25 ,10 )
+  @bar1 = Bar.new
+   
   end
 
   def test_bar_pricing
-    assert_equal(100,@bar1.drink1)
+    @bar1.add_drink_to_drinks_list(:something ,100)
+    @bar1.add_drink_to_drinks_list(:newdrink,50)
+    assert_equal(100,@bar1.drinks[:something])
   end 
 
   def test_bar_at_zero_profit 
     assert_equal(0,@bar1.bar_profits)
   end 
 
+  def test_bar_makes_sale
+    @bar1.add_drink_to_drinks_list(:something ,100)
+    @bar1.add_drink_to_drinks_list(:newdrink,50)
+    @bar1.adding_sale_to_profits(:something)
+    @bar1.adding_sale_to_profits(:newdrink)
+    assert_equal(150,@bar1.bar_profits)
+  end 
+
+
+
 end
+
+
+
+
